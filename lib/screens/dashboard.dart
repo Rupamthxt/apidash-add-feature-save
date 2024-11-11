c53773073564762854f57e11d653d26b556a1c17
Rupamthxt <rupamsaha365@gmail.com>
2024-11-11 18:27:22 +0530

    Implemented Save Shortcut.

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -21,21 +21,7 @@ class DesktopDashboard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Shortcuts(
-      shortcuts: {
-        LogicalKeySet(LogicalKeyboardKey.meta, LogicalKeyboardKey.keyS):
-            const SaveIntent(),
-        LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyS):
-            const SaveIntent(),
-      },
-      child: Actions(
-        actions: {
-          SaveIntent:
-              CallbackAction(onInvoke: (intent) => saveData(context, ref)),
-        },
-        child: const Dashboard(),
-      ),
-    );
+    return const Dashboard();
   }
 }
 
@@ -45,112 +31,125 @@ class Dashboard extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final railIdx = ref.watch(navRailIndexStateProvider);
-    return Scaffold(
-      body: SafeArea(
-        child: Row(
-          children: <Widget>[
-            Column(
-              children: [
-                SizedBox(
-                  height: kIsMacOS ? 32.0 : 16.0,
-                  width: 64,
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      isSelected: railIdx == 0,
-                      onPressed: () {
-                        ref.read(navRailIndexStateProvider.notifier).state = 0;
-                      },
-                      icon: const Icon(Icons.auto_awesome_mosaic_outlined),
-                      selectedIcon: const Icon(Icons.auto_awesome_mosaic),
-                    ),
-                    Text(
-                      'Requests',
-                      style: Theme.of(context).textTheme.labelSmall,
-                    ),
-                    kVSpacer10,
-                    IconButton(
-                      isSelected: railIdx == 1,
-                      onPressed: () {
-                        ref.read(navRailIndexStateProvider.notifier).state = 1;
-                      },
-                      icon: const Icon(Icons.laptop_windows_outlined),
-                      selectedIcon: const Icon(Icons.laptop_windows),
-                    ),
-                    Text(
-                      'Variables',
-                      style: Theme.of(context).textTheme.labelSmall,
-                    ),
-                    kVSpacer10,
-                    IconButton(
-                      isSelected: railIdx == 2,
-                      onPressed: () {
-                        ref.read(navRailIndexStateProvider.notifier).state = 2;
-                      },
-                      icon: const Icon(Icons.history_outlined),
-                      selectedIcon: const Icon(Icons.history_rounded),
-                    ),
-                    Text(
-                      'History',
-                      style: Theme.of(context).textTheme.labelSmall,
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
+    return Shortcuts(
+      shortcuts: {
+        LogicalKeySet(LogicalKeyboardKey.controlLeft, LogicalKeyboardKey.keyS): const SaveIntent(),
+      },
+      child: Actions(
+        actions: {
+          SaveIntent: CallbackAction(onInvoke: (intent) => saveData(context, ref))
+        },
+        child: FocusScope(
+          autofocus: true,
+          child: Scaffold(
+            body: SafeArea(
+              child: Row(
+                children: <Widget>[
+                  Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: NavbarButton(
-                          railIdx: railIdx,
-                          selectedIcon: Icons.help,
-                          icon: Icons.help_outline,
-                          label: 'About',
-                          showLabel: false,
-                          isCompact: true,
-                          onTap: () {
-                            showAboutAppDialog(context);
-                          },
-                        ),
+                      SizedBox(
+                        height: kIsMacOS ? 32.0 : 16.0,
+                        width: 64,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: NavbarButton(
-                          railIdx: railIdx,
-                          buttonIdx: 3,
-                          selectedIcon: Icons.settings,
-                          icon: Icons.settings_outlined,
-                          label: 'Settings',
-                          showLabel: false,
-                          isCompact: true,
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            isSelected: railIdx == 0,
+                            onPressed: () {
+                              ref.read(navRailIndexStateProvider.notifier).state = 0;
+                            },
+                            icon: const Icon(Icons.auto_awesome_mosaic_outlined),
+                            selectedIcon: const Icon(Icons.auto_awesome_mosaic),
+                          ),
+                          Text(
+                            'Requests',
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                          kVSpacer10,
+                          IconButton(
+                            isSelected: railIdx == 1,
+                            onPressed: () {
+                              ref.read(navRailIndexStateProvider.notifier).state = 1;
+                            },
+                            icon: const Icon(Icons.laptop_windows_outlined),
+                            selectedIcon: const Icon(Icons.laptop_windows),
+                          ),
+                          Text(
+                            'Variables',
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                          kVSpacer10,
+                          IconButton(
+                            isSelected: railIdx == 2,
+                            onPressed: () {
+                              ref.read(navRailIndexStateProvider.notifier).state = 2;
+                            },
+                            icon: const Icon(Icons.history_outlined),
+                            selectedIcon: const Icon(Icons.history_rounded),
+                          ),
+                          Text(
+                            'History',
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: NavbarButton(
+                                railIdx: railIdx,
+                                selectedIcon: Icons.help,
+                                icon: Icons.help_outline,
+                                label: 'About',
+                                showLabel: false,
+                                isCompact: true,
+                                onTap: () {
+                                  showAboutAppDialog(context);
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: NavbarButton(
+                                railIdx: railIdx,
+                                buttonIdx: 3,
+                                selectedIcon: Icons.settings,
+                                icon: Icons.settings_outlined,
+                                label: 'Settings',
+                                showLabel: false,
+                                isCompact: true,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            VerticalDivider(
-              thickness: 1,
-              width: 1,
-              color: Theme.of(context).colorScheme.surfaceContainerHighest,
-            ),
-            Expanded(
-              child: IndexedStack(
-                alignment: AlignmentDirectional.topCenter,
-                index: railIdx,
-                children: const [
-                  HomePage(),
-                  EnvironmentPage(),
-                  HistoryPage(),
-                  SettingsPage(),
+                  VerticalDivider(
+                    thickness: 1,
+                    width: 1,
+                    color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                  ),
+                  Expanded(
+                    child: IndexedStack(
+                      alignment: AlignmentDirectional.topCenter,
+                      index: railIdx,
+                      children: const [
+                        HomePage(),
+                        EnvironmentPage(),
+                        HistoryPage(),
+                        SettingsPage(),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
